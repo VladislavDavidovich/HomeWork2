d890f1fe580904f44031eaf15aa5252d2416d971
Vladislav <Vlad.davidovich.2014@mail.ru>
2022-08-14 12:01:13 +0300

    Выполненна вторая задача

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+string firstgNumber = Convert.ToString(number);
+
+if(firstgNumber.Length > 2)
+{
+    Console.WriteLine("третья цифра  " + firstgNumber[2]);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет " );
+}
